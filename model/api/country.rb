9582b769dec05a5a0b62f020394b53c315b8b988
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2010-11-05 18:40:48 -0500

    new api country

--- model/api/country.rb
+++ model/api/country.rb
@@ -5,14 +5,14 @@ module API
   class Country
     include HTTParty
     base_uri "http://spreadsheets.google.com/" #URL_CONFIG["api_url"]
-    default_params :pub => "1"
+    default_params :pub => "1",  :tqx => "responseHandler:callback"
 
     def self.get(*args)
-      Crack::JSON.parse((super(*args)))
+      Crack::JSON.parse((super(*args)).parsed_response[9..-3])
     end
 
     def self.getCountry(key)
-      get("/tq", :query => { :key => "#{key}" })
+      get("/tq", :query => { :key => "#{key}"})
     end
   end
 end
