0ba27ed7e27e0b8640adfbc8fe417a0d6aff4577
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2010-11-07 17:22:12 -0500

    pre parse instance country

--- model/country.rb
+++ model/country.rb
@@ -1,7 +1,17 @@
 require 'api/country'
 class Country
+  attr_accessor :id, :name, :states
+  def initialize(id, name, states)
+    @id = id
+    @name = name
+    @states = states
+  end
   def self.getCountry(key)
-    return API::Country.getCountry(key)
+    parse_instance(API::Country.getCountry(key),key)
+  end
+  protected
+  def self.parse_instance(attributes, id)
+    
   end
 end
 
